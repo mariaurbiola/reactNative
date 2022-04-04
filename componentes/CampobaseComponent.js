f5f7f452ffe6e897674cb2fa877ba6f7c70c03fc
María <urbiola.121411@e.unavarra.es>
2022-04-04 16:41:43 +0200

    Stack Navigation

--- componentes/CampobaseComponent.js
+++ componentes/CampobaseComponent.js
@@ -1,31 +1,54 @@
-import React, { Component } from 'react'; 
-import Calendario from './CalendarioComponent'; 
-import { EXCURSIONES } from '../comun/excursiones'; 
+import React, { Component } from 'react';
+import Constants from 'expo-constants';
+import Calendario from './CalendarioComponent';
 import DetalleExcursion from './DetalleExcursionComponent';
 import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+const Stack = createStackNavigator();
 
-class Campobase extends Component { 
-    constructor(props) { 
-        super(props); 
-        this.state = {
-            excursiones: EXCURSIONES,
-            seleccionExcursion: null
-          };
-      
-    } 
-    onSeleccionExcursion(excursionId) {
-        this.setState({seleccionExcursion: excursionId})
-    }  
-    
-    render() 
-    
-    { return (
-        <View>
-            <DetalleExcursion excursion={this.state.excursiones.filter((excursion) => excursion.id === this.state.seleccionExcursion)[0]} />
-            <Calendario excursiones={this.state.excursiones} onPress={( excursionId) => this. onSeleccionExcursion(excursionId)} />
-        </View>        
+function CalendarioNavegador() {
+  return (
+    <Stack.Navigator    //aqui se configura la barra de arriba
+      initialRouteName="Calendario"
+      screenOptions={{
+        headerMode: 'float',
+        headerTintColor: '#fff',    //color de la flecha de back - blanco
+        headerStyle: { backgroundColor: '#015afc' },    //color del fondo de la barra - azul
+        headerTitleStyle: { color: '#fff' },    //color del titulo - blasnco
+      }}
+    >
+      <Stack.Screen //cada una de las pestañas
+        name="Calendario"
+        component={Calendario}
+        options={{
+          title: 'Calendario Gaztaroa',
+        }}
+      />
+      <Stack.Screen
+        name="DetalleExcursion"
+        component={DetalleExcursion}
+        options={{
+          title: 'Detalle Excursión',
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+class Campobase extends Component {
+
+  render() {
+
+    return (
+      <NavigationContainer>
+        <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
+          <CalendarioNavegador />
+        </View>
+      </NavigationContainer>   
     );
-    }
+}
+}
 
-} export default Campobase;
+export default Campobase;
